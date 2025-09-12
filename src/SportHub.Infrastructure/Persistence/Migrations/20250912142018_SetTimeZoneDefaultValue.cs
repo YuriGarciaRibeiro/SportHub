305ee2bbdc55c53544f6a42197dcdfda3277d7ae
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2025-09-12 11:40:44 -0300

    feat: Set default value for TimeZone in Establishments to "America/Maceio"

---
+++ src/SportHub.Infrastructure/Persistence/Migrations/20250912142018_SetTimeZoneDefaultValue.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportHub.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetTimeZoneDefaultValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TimeZone",
+                table: "Establishments",
+                type: "text",
+                nullable: false,
+                defaultValue: "America/Maceio",
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TimeZone",
+                table: "Establishments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldDefaultValue: "America/Maceio");
+        }
+    }
+}
